b72c9913fb1729876283e1b51ad59f23d56b4354
stripesfensky <88601301+stripesfensky@users.noreply.github.com>
2026-07-12 22:16:49 -0400

    Update treehut.js

--- scripts/treehut.js
+++ scripts/treehut.js
@@ -18,17 +18,42 @@ gci.addEventListener("change", (event) => {
     const fileBuffer = event.target.result;
     const fileArray = new Uint8Array(fileBuffer);
 
+    let acStr = "Animal Crossing (USA)";
+
     let gafStr = getHexString(fileArray, "00000000", "00000005");
-    let gafTest = RegExp("GAF\[A-Z]01").test(gafStr);
+    let gafTest = RegExp("GA\[A-Z]\[A-Z]01").test(gafStr);
     let muraStr = getHexString(fileArray, "00000008", "0000001A");
-    let muraTest = RegExp("DobutsunomoriP_MURA").test(muraStr);
-    
+    let muraTest = RegExp("Dobutsunomori\[A-Z]_MURA").test(muraStr);
+
+    switch(gafStr) {
+      case "GAFJ01":
+        acStr = "Dōbutsu no Mori+";
+        break;
+      case "GAEJ01":
+        acStr = "Dōbutsu no Mori e+";
+        break;
+      case "GAFP01":
+        acStr = "Animal Crossing (EUR)";
+        break;
+      case "GAFU01":
+        acStr = "Animal Crossing (AUS)";
+        break;
+    }
+        
     if (gafTest == false || muraTest == false) {
       setFileError("This file is invalid.", "red");
     }
     else {
-      setFileError("This file is valid.", "green");
-      getAcreHex(fileArray, "0003D3E8", "0003D473");
+      setFileError("This file is valid. (" + acStr + " / " + gafStr + ", " + muraStr + ")", "green");
+      if (gafStr == "GAEJ01") {
+        getAcreHex(fileArray, "0002C100", "0002C18B");
+      } 
+      else if (gafStr == "GAFJ01") {
+        getAcreHex(fileArray, "00015F28", "00015FB3");
+      } 
+      else {
+        getAcreHex(fileArray, "0003D3E8", "0003D473");
+      }
     }
 
     return;
@@ -67,7 +92,7 @@ function getAcreHex(array, startHex, endHex) {
   const sliced = getSlicedArray(array, startHex, endHex);
   
   if (sliced.length / 2 != 70) {
-    setFileError("The acre data for this save file is invalid.");
+    setFileError("The acre data for this save file is invalid.", "red");
     return;
   }
 
@@ -77,7 +102,7 @@ function getAcreHex(array, startHex, endHex) {
   for (let i = 0; i < sliced.length; i += 2) {
     const acreFirst = sliced[i].toString(16).padStart(2, "0").toUpperCase();
     const acreSecond = sliced[i + 1].toString(16).padStart(2, "0").toUpperCase();
-    acreHex[acreHexIdx] = `0x${acreFirst}${acreSecond}`;
+    acreHex[acreHexIdx] = "0x" + acreFirst + acreSecond;
     acreHexIdx += 1;
   }
 
@@ -85,19 +110,18 @@ function getAcreHex(array, startHex, endHex) {
 
   acreHexIdx = 0;
 
-  let table = document.createElement("table");
+  let mapGrid = document.createElement("div");
+  mapGrid.id = "mapGrid"
 
-  for (let mapRow = 0; mapRow < 10; mapRow++) {
-    let row = table.insertRow(-1);
-    
-    for (let mapCol = 0; mapCol < 7; mapCol++) {
-      let rowCol = row.insertCell(-1);
-      rowCol.innerText = acreHex[acreHexIdx];
-      acreHexIdx++;
-    }
+  for (let i = 0; i < acreHex.length; i++) {
+    let acre = document.createElement("div");
+    acre.className = "acre";
+    acre.innerText = acreHex[acreHexIdx];
+    mapGrid.append(acre);
+    acreHexIdx++;
   }
 
   map.innerHTML = "<h2>Map</h2>"
-  map.appendChild(table);
+  map.appendChild(mapGrid);
   return;
 }
