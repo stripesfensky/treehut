c496ba71a48d9e77392ee5b14ba5b4f1b8fd4f9e
stripesfensky <88601301+stripesfensky@users.noreply.github.com>
2026-08-05 21:54:29 -0400

    Update gciUtils.js

--- scripts/inc/gciUtils.js
+++ scripts/inc/gciUtils.js
@@ -37,6 +37,8 @@ export async function loadGci(gciUpload) {
         break;
       case "GADEXX":
         gameStr = "Animal Crossing Deluxe v41.0+";
+        gciSaveOffset = 0x10040;
+        saveAcreOffset = 0x173A0;
         break;
     }
 
